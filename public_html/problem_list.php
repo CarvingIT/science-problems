35fc7c943945b5b26e88806ef18f436ef9e72fc6
Ketan Kulkarni <ketan@carvingit.com>
2014-01-22 14:47:38 +0530

    added print and share button on the list page

--- public_html/problem_list.php
+++ public_html/problem_list.php
@@ -44,7 +44,15 @@
                     if($problem_count > 0){ 
                         if(!empty($_GET['u'])){ 
                   ?>
+                  <a class="no-print" href="javascript:window.print();" title="Print this list"><img src="/images/print.png"/></a>
                   <a class="no-print" href="/set_list.php?list_path=<?php echo $_SERVER['REQUEST_URI']; ?>" title="Play this list"><img src="/images/play.png"/></a>
+                  <div class="sharebutton">
+                  <!-- AddThis Button BEGIN -->
+                  <a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=300&amp;pubid=ra-51b3ffe250a8cff5"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+                  <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51b3ffe250a8cff5"></script>
+                  <!-- AddThis Button END -->
+                  </div>
                   <?php }} else{ ?>
                     There currently are no problems in this list.
                   <?php } ?>
